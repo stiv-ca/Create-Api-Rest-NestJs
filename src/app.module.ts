ff1ea78db4411f929b3fcf4c65bbbf88581a5f99
Stiven <74148301+stiv-ca@users.noreply.github.com>
2024-05-31 11:45:18 -0500

    Use of design pattern and entity creation.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,  // Asegúrese de que coincide con el puerto expuesto en Docker o su configuración local
+      username: 'user_crud',
+      password: 'root',
+      database: 'db_crud',
+      autoLoadEntities: true,
+      synchronize: true  // No usar en producción, puede perder datos
+    }),
+  ],
 })
 export class AppModule {}
--- src/cats/entities/cat.entity.ts
+++ src/cats/entities/cat.entity.ts
@@ -1,1 +1,21 @@
-export class Cat {}
+import { Column, DeleteDateColumn, Entity } from "typeorm"
+
+@Entity()
+export class Cat {
+
+    @Column({primary: true, generated: true})
+    id: number;
+
+    @Column() 
+    name: string;
+    
+    @Column()
+    age: number;
+
+    @Column()
+    breed: string;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+
+}
